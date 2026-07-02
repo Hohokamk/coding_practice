4bb5b0e940581570686b6dc5f5c1c75fb480428f
Hohokamk <wangguancheen@gmail.com>
2026-07-02 11:23:21 +0800

    新增两道算法题

---
+++ 算法课/迷雾森林.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int w, h;
+char fogforest[23][23];
+int res = 0;
+int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
+int dfs(int x, int y)
+{
+    
+}
+
+int main()
+{
+    while (cin >> w >> h, w || h)
+    {
+        int startx, starty;
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                cin >> fogforesrt[i][j];
+                if (fogforest[i][j] == '@')
+                {
+                    startx = i;
+                    starty = j;
+                }
+            }
+        }
+    }
+    cout << dfs(startx, starty) << endl;
+}
